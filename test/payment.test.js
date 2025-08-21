ac52bea7f9893f9baf0aad4d70574c716128bcf8
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2025-08-21 10:14:42 +0100

    update error

--- test/payment.test.js
+++ test/payment.test.js
@@ -1,13 +1,9 @@
+require("dotenv").config();
 const request = require("supertest");
 const app = require("../src/app");
 const mongoose = require("mongoose");
-const axios = require("axios");
-
-// Mock axios so Paystack calls don't actually run
-jest.mock("axios");
 
 beforeAll(async () => {
-  // connect to test DB
   await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
 });
 
@@ -19,23 +15,12 @@ describe("Payment API", () => {
   let paymentReference;
 
   it("POST /api/v1/payments should initiate a payment", async () => {
-    // Mock the Paystack response
-    axios.post.mockResolvedValue({
-      data: {
-        data: {
-          authorization_url: "https://paystack.com/fake-url"
-        }
-      }
+    const res = await request(app).post("/api/v1/payments").send({
+      firstName: "Test",
+      lastName: "User",
+      email: "test@example.com",
+      amount: 1000,
     });
-
-    const res = await request(app)
-      .post("/api/v1/payments")
-      .send({
-        firstName: "Test",
-        lastName: "User",
-        email: "test@example.com",
-        amount: 1000
-      });
 
     expect(res.statusCode).toBe(201);
     expect(res.body.payment).toHaveProperty("reference");
